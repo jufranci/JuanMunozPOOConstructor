41ef25bf4f1d9e66242593f4039fd556b726b3a7
Francisco <jufranci@gmail.com>
2020-09-02 19:15:32 -0300

    3rd commit
    Se agrega constructor nuevo y variable edad

--- src/juanmunoz/pooconstructor/JuanMunozPOOConstructor.java
+++ src/juanmunoz/pooconstructor/JuanMunozPOOConstructor.java
@@ -1,15 +1,17 @@
 package juanmunoz.pooconstructor;
 public class JuanMunozPOOConstructor {
     public static void main(String[] args) {
-        persona p0,p1,p2,p3;
+        persona p0,p1,p2,p3,p4;
         p0 = new persona();
         p1 = new persona (24356123);
         p2 = new persona (12345678, "Dario");
         p3 = new persona (98765432, "Esteban", "Rodriguez");
+        p4 = new persona (31264598, "Lucas", "Lopez", 24);
         p0.imprimir();
         p1.imprimir();
         p2.imprimir();
         p3.imprimir();
+        p4.imprimir();
     }
     
 }
--- src/juanmunoz/pooconstructor/persona.java
+++ src/juanmunoz/pooconstructor/persona.java
@@ -1,11 +1,15 @@
 package juanmunoz.pooconstructor;
 public class persona {
-    int dni = 0;
+    int dni = 0, edad = 0;
     String nombre="", apellido="";
     public persona(){
-        System.out.println("Creando persona...");
+        this.dni = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.edad = 0;
     }
     public persona(int dni){
+        this();
         this.dni = dni;        
     }
     public persona (int dni, String nombre){
@@ -16,12 +20,18 @@ public class persona {
         this(dni,nombre);
         this.apellido = apellido;
     }
+    public persona (int dni, String nombre, String apellido, int edad){
+        this(dni,nombre,apellido);
+        this.edad = edad;
+    }
     public void imprimir(){
-        if(dni !=0 && !nombre.equals("") && !apellido.equals("")){
+        if(dni !=0 && !nombre.equals("") && !apellido.equals("") && edad !=0){
+            System.out.println("DNI N°" + dni + "\nNombre y apellido: " + nombre + " " + apellido + "\nEdad: " + edad);
+        } else if (dni !=0 && !nombre.equals("") && !apellido.equals("")){
             System.out.println("DNI N°" + dni + "\nNombre y apellido: " + nombre + " " + apellido);
-        } else if (dni !=0 && !nombre.equals("")){
+        } else if (dni !=0 && !nombre.equals("")) {
             System.out.println("DNI N°" + dni + "\nNombre: " + nombre);
-        } else if (dni !=0) {
+        } else if (dni !=0){
             System.out.println("DNI N°" + dni);
         }
         
